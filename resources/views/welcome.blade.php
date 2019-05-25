980e03a82c309df3d731bdd747bd8dfebd8fb940
royhan31 <izzapark34@gmail.com>
2019-05-25 17:34:43 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Plugin Home
                 </div>
 
                 <div class="links">
